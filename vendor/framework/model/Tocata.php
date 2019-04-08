02480c393b05f389abc365ea8d7edfcda3a83752
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2019-04-07 22:28:22 -0300

    Método para salvar um registro no banco criado

--- vendor/framework/model/Tocata.php
+++ vendor/framework/model/Tocata.php
@@ -12,7 +12,23 @@
          */
         public static function listAll() {
             $db = new Database();
-            return $db->select("SELECT * FROM tb_tocatas ORDER BY data DESC");
+            return $db->select("SELECT * FROM tb_tocatas ORDER BY data_tocata DESC, horario DESC");
+        }
+
+        /**
+         * Salva uma tocata no banco de dados
+         */
+        public function save() {
+            $db = new Database();
+            $results = $db->select("CALL sp_save_tocatas (:local, :data, :horario, :observacoes, :cadastrado_por, :data_cadastro)", array(
+                ":local" => $this->getLocal(),
+                ":data" => $this->getData_Tocata(),
+                ":horario" => $this->getHorario(),
+                ":observacoes" => $this->getObservacoes(),
+                ":cadastrado_por" => $this->getCadastrado_Por(),
+                ":data_cadastro" => $this->getData_Cadastro()
+            ));
+            $this->setData($results[0]);
         }
     }
 ?>
