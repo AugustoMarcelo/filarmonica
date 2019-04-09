3c8768ba32bb8bb3b5f2b518d09388734e33426f
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2019-04-08 21:43:04 -0300

    Métodos get, update e delete adicionados

--- vendor/framework/model/Tocata.php
+++ vendor/framework/model/Tocata.php
@@ -30,5 +30,34 @@
             ));
             $this->setData($results[0]);
         }
+
+        /**
+         * Busca por uma tocata de acordo com o ID passado
+         * @param id ID da tocata a ser buscada no banco de dados
+         */
+        public function get($id) {
+            $db = new Database();
+            $results = $db->select("SELECT * FROM tb_tocatas WHERE id = :id", [":id" => $id]);
+            $this->setData($results[0]);
+        }
+
+        public function update() {
+            $db = new Database();
+            $results = $db->select("CALL sp_update_tocatas (:param_id, :param_local, :param_data_tocata, :param_horario, :param_observacoes, :param_atualizado_por, :param_data_atualizacao)", array(
+                ":param_id" => $this->getId(),
+                ":param_local" => $this->getLocal(),
+                ":param_data_tocata" => $this->getData_Tocata(),
+                ":param_horario" => $this->getHorario(),
+                ":param_observacoes" => $this->getObservacoes(),
+                ":param_atualizado_por" => $this->getAtualizado_Por(),
+                ":param_data_atualizacao" => $this->getData_Atualizacao()
+            ));
+            $this->setData($results[0]);
+        }
+
+        public function delete() {
+            $db = new Database();
+            $db->query("DELETE FROM tb_tocatas WHERE id = :id", [":id" => $this->getId()]);
+        }
     }
 ?>
