5f78db4f33a32d1358aebfd8813a69bedc36ac3a
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2019-04-08 21:44:53 -0300

    Rotas para adicionar tocata e lista de chamada criadas

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     use Framework\model\User;
     use Framework\model\Componente;
     use Framework\model\Tocata;
+    use Framework\model\Frequencia;
     use Framework\utils\DateUtils;
 
     $app = new Slim();
@@ -112,7 +113,7 @@
     $app->post('/componentes/cadastrar', function() {
         User::verifyLogin();
         $componente = new Componente();
-        $_POST['cadastrado_por'] = $_SESSION[User::SESSION] ?? 1;
+        $_POST['cadastrado_por'] = $_SESSION[User::SESSION] ? $_SESSION[User::SESSION]['id'] : 1;
         $_POST['data_cadastro'] = DateUtils::now("Y-m-d H:i:s");
         $_POST['ativo'] = isset($_POST['ativo']) ? 1 : 0;
         $componente->setData($_POST);
@@ -144,7 +145,7 @@
         User::verifyLogin();
         $componente = new Componente();
         $_POST['ativo'] = isset($_POST['ativo']) ? 1 : 0;
-        $_POST['atualizado_por'] = $_SESSION[User::SESSION] ?? 1;
+        $_POST['atualizado_por'] = $_SESSION[User::SESSION] ? $_SESSION[User::SESSION]['id'] : 1;
         $_POST['data_atualizacao'] = DateUtils::now("Y-m-d H:i:s");
         $componente->get((int) $id);
         $componente->setData($_POST);
@@ -199,12 +200,126 @@
     // ROTA PARA CADASTRAR UMA TOCATA
     $app->post('/tocatas/cadastrar', function() {
         User::verifyLogin();
+        $_POST['cadastrado_por'] = $_SESSION[User::SESSION] ? $_SESSION[User::SESSION]['id'] : 1;
+        $_POST['data_cadastro'] = DateUtils::now("Y-m-d H:i:s");
+        $tocata = new Tocata();
+        $tocata->setData($_POST);
+        $tocata->save();
+        header("Location: /tocatas");
+        exit;
+        // foreach ($_POST as $key => $value) {
+        //     $id = explode('_', $key);
+        //     if ($id[0] == 'componente') {
+        //         var_dump($id[1]);
+        //     }
+        // }
+        // exit;
+    });
+
+    // ROTA PARA ATUALIZAR UMA TOCATA
+    $app->get('/tocatas/editar/:idtocata', function($idtocata) {
+        User::verifyLogin();
+        $tocata = new Tocata();
+        $tocata->get((int) $idtocata);
+        $page = new Page(array(
+            "data" => array(
+                "componentesClassActive" => "",
+                "userClassActive" => "",
+                "frequenciasClassActive" => "",
+                "tocatasClassActive" => "active"
+            )
+        ));
+        $page->setTemplate("tocatas-update", array(
+            "tocata" => $tocata->getData()
+        ));
+    });
+
+    // ROTA PARA ATUALIZAR UMA TOCATA
+    $app->post('/tocatas/editar/:idtocata', function($idtocata) {
+        User::verifyLogin();
+        $tocata = new Tocata();
+        $_POST['atualizado_por'] = isset($_SESSION[User::SESSION]) ? $_SESSION[User::SESSION]['id'] : 1;
+        $_POST['data_atualizacao'] = DateUtils::now('Y-m-d H:i:s');
+        $tocata->get((int) $idtocata);
+        $tocata->setData($_POST);
+        $tocata->update();
+        header("Location: /tocatas");
+        exit;
+    });
+
+    // ROTA PARA REMOVER UMA TOCATA
+    $app->get('/tocatas/delete/:idtocata', function($idtocata) {
+        $tocata = new Tocata();
+        $tocata->get((int) $idtocata);
+        $tocata->delete();
+        header("Location: /tocatas");
+        exit;
+    });
+
+    // ROTA PARA LISTA DE CHAMADA
+    $app->get('/tocatas/:idtocata/chamada', function($idtocata) {
+        $frequencia = new Frequencia();
+        $frequencia->get((int) $idtocata);
+        if (count($frequencia->getPresencas()) > 0) {
+            header("Location: /tocatas/editar/$idtocata/chamada");
+            exit;
+        }
+        $componentes = Componente::listAll();
+        $tocata = new Tocata();
+        $tocata->get((int) $idtocata);
+        $page = new Page(array(
+            "data" => array(
+                "componentesClassActive" => "",
+                "userClassActive" => "",
+                "frequenciasClassActive" => "",
+                "tocatasClassActive" => "active"
+            )
+        ));
+        $page->setTemplate('chamada', array(
+            "componentes" => $componentes,
+            "tocata" => $tocata->getData(),
+            "presencas" => $frequencia->getPresencas()
+        ));
+    });
+
+    $app->get('/tocatas/editar/:idtocata/chamada', function($idtocata) {
+        $frequencia = new Frequencia();
+        $frequencia->get((int) $idtocata);
+        $componentes = Componente::listAll();
+        $tocata = new Tocata();
+        $tocata->get((int) $idtocata);
+        $page = new Page(array(
+            "data" => array(
+                "componentesClassActive" => "",
+                "userClassActive" => "",
+                "frequenciasClassActive" => "",
+                "tocatasClassActive" => "active"
+            )
+        ));
+        $page->setTemplate('chamada-update', array(
+            "componentes" => $componentes,
+            "tocata" => $tocata->getData(),
+            "presencas" => $frequencia->getPresencas()
+        ));
+    });
+
+    // ROTA PARA CADASTRAR UMA CHAMADA
+    $app->post('/tocatas/:idtocata/chamada', function($idtocata) {
+        $frequencia = new Frequencia();
+        $frequencia->setTocata_ID($idtocata);
+        // REMOVE TODA A LISTA DE CHAMADA DA TOCATA ANTES
+        $frequencia->delete();
+        $frequencia->setPresenca(1);
+        $frequencia->setCadastrado_Por(isset($_SESSION[User::SESSION]) ? $_SESSION[User::SESSION]['id'] : 1);
+        $frequencia->setData_Cadastro(DateUtils::now('Y-m-d H:i:s'));
         foreach ($_POST as $key => $value) {
-            $id = explode('_', $key);
-            if ($id[0] == 'componente') {
-                var_dump($id[1]);
+            $field = explode('_', $key);
+            if ($field[0] == 'componente') {
+                $frequencia->setComponente_ID($field[1]);
+                $frequencia->save();
             }
         }
+        header("Location: /tocatas");
         exit;
     });
 
