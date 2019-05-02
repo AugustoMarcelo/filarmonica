a13c8f3c37ee424b34b363e1110287f209762191
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2019-05-01 21:45:39 -0300

    Método para adicionar a falta

--- vendor/framework/model/Frequencia.php
+++ vendor/framework/model/Frequencia.php
@@ -19,9 +19,29 @@
             ));
         }
 
+        /**
+         * Informa a falta dos componentes
+         */
+        public function setFaults() {
+            $db = new Database();
+            // Seleciona todos os componentes que faltaram
+            $results = $db->select("SELECT id FROM tb_componentes WHERE ativo = 1 AND id NOT IN (SELECT componente_id FROM tb_frequencias WHERE tocata_id = :tocata)", [":tocata" => $this->getTocata_ID()]);
+            // Insere a falta para os componentes que faltaram
+            foreach ($results as $result) {
+                $db->query("CALL sp_save_frequencia (:param_tocata, :param_componente, :param_presenca, :param_cadastrado_por, :param_data_cadastro)", array(
+                    ":param_tocata" => $this->getTocata_ID(),
+                    ":param_componente" => $result['id'],
+                    ":param_presenca" => 0,
+                    ":param_cadastrado_por" => $this->getCadastrado_Por(),
+                    ":param_data_cadastro" => $this->getData_Cadastro()
+                ));
+            }
+        }
+
         public function get($id) {
             $db = new Database();
-            $results = $db->select("SELECT c.nome, c.id, IF(f.presenca IS NULL, 0, 1) AS presenca FROM tb_frequencias f RIGHT JOIN tb_componentes c ON f.componente_id = c.id AND f.tocata_id = :id OR f.tocata_id IS NULL WHERE c.ativo = 1 ORDER BY c.nome", [":id" => $id]);
+            // $results = $db->select("SELECT c.nome, c.id, IF(f.presenca IS NULL, 0, 1) AS presenca FROM tb_frequencias f RIGHT JOIN tb_componentes c ON f.componente_id = c.id AND f.tocata_id = :id OR f.tocata_id IS NULL WHERE c.ativo = 1 ORDER BY c.nome", [":id" => $id]);
+            $results = $db->select("SELECT c.nome, c.id, f.presenca FROM tb_frequencias f INNER JOIN tb_componentes c ON f.componente_id = c.id AND f.tocata_id = :id ORDER BY c.nome", [":id" => $id]);
             foreach ($results as $result) {
                 $this->presencas[] = $result;
             }
