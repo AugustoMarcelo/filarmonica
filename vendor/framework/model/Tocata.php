8254eb7652b4d035eb11b7f96ed10be58447adea
AugustoMarcelo <mrclgst10@gmail.com>
2019-06-16 00:06:03 -0300

    Query de faltas mensais agora retorna a quantidade atrasos

--- vendor/framework/model/Tocata.php
+++ vendor/framework/model/Tocata.php
@@ -72,7 +72,7 @@
 
         public static function getFaultsForMonth($month) {
             $db = new Database();
-            return $db->select("SELECT c.nome, :month AS month, COUNT(t.id) AS qtde_tocatas, SUM(IF(f.presenca = 0 OR f.presenca IS NULL, 1, 0)) AS faltas FROM tb_frequencias f RIGHT JOIN tb_tocatas t ON f.tocata_id = t.id OR f.tocata_id IS NULL INNER JOIN tb_componentes c ON f.componente_id = c.id OR f.componente_id IS NULL WHERE DATE_FORMAT(t.data_tocata,'%Y-%m') = :month AND c.ativo = 1 GROUP BY c.nome ORDER BY c.nome", [
+            return $db->select("SELECT c.nome, :month AS month, COUNT(t.id) AS qtde_tocatas, SUM(IF(f.presenca = 0 OR f.presenca IS NULL, 1, 0)) AS faltas, SUM(IF(f.presenca = 3, 1, 0)) AS atrasos FROM tb_frequencias f RIGHT JOIN tb_tocatas t ON f.tocata_id = t.id OR f.tocata_id IS NULL INNER JOIN tb_componentes c ON f.componente_id = c.id OR f.componente_id IS NULL WHERE DATE_FORMAT(t.data_tocata,'%Y-%m') = :month AND c.ativo = 1 GROUP BY c.nome ORDER BY c.nome", [
                 ":month" => $month
             ]);
         }
